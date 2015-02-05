e7e39c350614ff497eb4acaa2f81cf238eedbe83
Nadia Dencheva <nadia.dencheva@gmail.com>
2015-02-05 09:47:53 -0500

    add back a dropped test

--- pyasdf/tags/transform/tests/test_transform.py
+++ pyasdf/tags/transform/tests/test_transform.py
@@ -17,6 +17,7 @@ test_models = [astmodels.Identity(2), astmodels.Polynomial1D(2, c0=1, c1=2, c2=3
 def test_transforms_compound(tmpdir):
     tree = {
         'compound':
+            astmodels.Shift(1) & astmodels.Shift(2) |
             astmodels.Sky2Pix_TAN() |
             astmodels.Rotation2D() |
             astmodels.AffineTransformation2D([[2, 0], [0, 2]], [42, 32]) +
@@ -24,6 +25,23 @@ def test_transforms_compound(tmpdir):
     }
 
     helpers.assert_roundtrip_tree(tree, tmpdir)
+
+
+def test_inverse_transforms(tmpdir):
+    rotation = astmodels.Rotation2D(32)
+    rotation.inverse = astmodels.Rotation2D(45)
+
+    real_rotation = astmodels.Rotation2D(32)
+
+    tree = {
+        'rotation': rotation,
+        'real_rotation': real_rotation
+        }
+
+    def check(ff):
+        assert ff.tree['rotation'].inverse.angle == 45
+
+    helpers.assert_roundtrip_tree(tree, tmpdir, check)
 
 
 @pytest.mark.parametrize(('model'), test_models)
