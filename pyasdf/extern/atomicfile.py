5342e0753f276f5bf2892c44269c85fb92706f10
Michael Droettboom <mdboom@gmail.com>
2015-04-23 11:18:55 -0400

    Don't have empty exceptions.

--- pyasdf/extern/atomicfile.py
+++ pyasdf/extern/atomicfile.py
@@ -2,68 +2,64 @@ import os
 import tempfile
 import sys
 import errno
-import time
-import random
 
 
-can_rename_open_file = False
 if os.name == 'nt': # pragma: no cover
+    import random
+    import time
+
     _rename = lambda src, dst: False
     _rename_atomic = lambda src, dst: False
 
-    try:
-        import ctypes
+    import ctypes
 
-        _MOVEFILE_REPLACE_EXISTING = 0x1
-        _MOVEFILE_WRITE_THROUGH = 0x8
-        _MoveFileEx = ctypes.windll.kernel32.MoveFileExW
+    _MOVEFILE_REPLACE_EXISTING = 0x1
+    _MOVEFILE_WRITE_THROUGH = 0x8
+    _MoveFileEx = ctypes.windll.kernel32.MoveFileExW
 
-        def _rename(src, dst):
-            if not isinstance(src, unicode):
-                src = unicode(src, sys.getfilesystemencoding())
-            if not isinstance(dst, unicode):
-                dst = unicode(dst, sys.getfilesystemencoding())
-            if _rename_atomic(src, dst):
-                return True
+    def _rename(src, dst):
+        if not isinstance(src, unicode):
+            src = unicode(src, sys.getfilesystemencoding())
+        if not isinstance(dst, unicode):
+            dst = unicode(dst, sys.getfilesystemencoding())
+        if _rename_atomic(src, dst):
+            return True
+        retry = 0
+        rv = False
+        while not rv and retry < 100:
+            rv = _MoveFileEx(src, dst, _MOVEFILE_REPLACE_EXISTING |
+                                       _MOVEFILE_WRITE_THROUGH)
+            if not rv:
+                time.sleep(0.001)
+                retry += 1
+        return rv
+
+    # new in Vista and Windows Server 2008
+    _CreateTransaction = ctypes.windll.ktmw32.CreateTransaction
+    _CommitTransaction = ctypes.windll.ktmw32.CommitTransaction
+    _MoveFileTransacted = ctypes.windll.kernel32.MoveFileTransactedW
+    _CloseHandle = ctypes.windll.kernel32.CloseHandle
+
+    def _rename_atomic(src, dst):
+        ta = _CreateTransaction(None, 0, 0, 0, 0, 1000, 'Atomic rename')
+        if ta == -1:
+            return False
+        try:
             retry = 0
             rv = False
             while not rv and retry < 100:
-                rv = _MoveFileEx(src, dst, _MOVEFILE_REPLACE_EXISTING |
-                                           _MOVEFILE_WRITE_THROUGH)
-                if not rv:
+                rv = _MoveFileTransacted(src, dst, None, None,
+                                         _MOVEFILE_REPLACE_EXISTING |
+                                         _MOVEFILE_WRITE_THROUGH, ta)
+                if rv:
+                    rv = _CommitTransaction(ta)
+                    break
+                else:
                     time.sleep(0.001)
                     retry += 1
             return rv
-
-        # new in Vista and Windows Server 2008
-        _CreateTransaction = ctypes.windll.ktmw32.CreateTransaction
-        _CommitTransaction = ctypes.windll.ktmw32.CommitTransaction
-        _MoveFileTransacted = ctypes.windll.kernel32.MoveFileTransactedW
-        _CloseHandle = ctypes.windll.kernel32.CloseHandle
-        can_rename_open_file = True
-
-        def _rename_atomic(src, dst):
-            ta = _CreateTransaction(None, 0, 0, 0, 0, 1000, 'Atomic rename')
-            if ta == -1:
-                return False
-            try:
-                retry = 0
-                rv = False
-                while not rv and retry < 100:
-                    rv = _MoveFileTransacted(src, dst, None, None,
-                                             _MOVEFILE_REPLACE_EXISTING |
-                                             _MOVEFILE_WRITE_THROUGH, ta)
-                    if rv:
-                        rv = _CommitTransaction(ta)
-                        break
-                    else:
-                        time.sleep(0.001)
-                        retry += 1
-                return rv
-            finally:
-                _CloseHandle(ta)
-    except Exception:
-        pass
+        finally:
+            _CloseHandle(ta)
 
     def atomic_rename(src, dst):
         # Try atomic or pseudo-atomic rename
@@ -84,7 +80,6 @@ if os.name == 'nt': # pragma: no cover
                 pass
 else:
     atomic_rename = os.rename
-    can_rename_open_file = True
 
 
 class _AtomicWFile(object):
