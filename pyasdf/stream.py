3f0ecb8bc0e50420ecb82717f4f0b465913f28fa
Michael Droettboom <mdboom@gmail.com>
2015-04-29 21:20:59 -0400

    Fix Python 3.x

--- pyasdf/stream.py
+++ pyasdf/stream.py
@@ -22,7 +22,8 @@ class Stream(ndarray.NDArrayType):
          >>> with open('test.asdf', 'wb') as fd:
          ...     ff.write_to(fd)
          ...     for i in range(200):
-         ...         fd.write(np.array([i] * 1024, np.float64).tostring())
+         ...         nbytes = fd.write(
+         ...                      np.array([i] * 1024, np.float64).tostring())
     """
 
     types = []
