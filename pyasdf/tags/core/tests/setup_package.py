21fd3f46ae35f0228012de1d5e5bf198f73f5cfb
Michael Droettboom <mdboom@gmail.com>
2015-05-26 12:53:15 -0400

    Add missing test data

---
+++ pyasdf/tags/core/tests/setup_package.py
@@ -0,0 +1,9 @@
+# Licensed under a 3-clause BSD style license - see LICENSE.rst
+# -*- coding: utf-8 -*-
+
+from __future__ import absolute_import, division, unicode_literals, print_function
+
+
+def get_package_data():  # pragma: no cover
+    return {
+        str(_ASTROPY_PACKAGE_NAME_ + '.tags.core.tests'): ['data/*.yaml']}
