eccfdb366f9555662f9902e49158b765fde9930a
Erik M. Bray <embray@stsci.edu>
2016-01-08 15:08:56 -0500

    Finishing v1.0.1 release

--- setup.py
+++ setup.py
@@ -48,7 +48,7 @@ LONG_DESCRIPTION = package.__doc__
 builtins._PACKAGE_NAME_ = 'pyasdf'
 
 # VERSION should be PEP386 compatible (http://www.python.org/dev/peps/pep-0386)
-VERSION = '1.0.1'
+VERSION = '1.0.2.dev'
 
 # Indicates if this version is a release version
 RELEASE = 'dev' not in VERSION
