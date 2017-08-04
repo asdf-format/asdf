474701eedc969455c4accd466936beb0f5dee738
Dan D'Avella <ddavella@stsci.edu>
2017-08-04 11:32:36 -0400

    Add "unit tests" for diff tool to increase coverage

---
+++ asdf/commands/tests/setup_package.py
@@ -0,0 +1,9 @@
+# Licensed under a 3-clause BSD style license - see LICENSE.rst
+# -*- coding: utf-8 -*-
+
+from __future__ import absolute_import, division, unicode_literals, print_function
+
+
+def get_package_data():  # pragma: no cover
+    return {
+        str(_PACKAGE_NAME_ + '.commands.tests'): ['data/*.asdf', 'data/*.diff']}
