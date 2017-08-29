c2aea782dc8d81c6781f567c1d77abd92a573824
Dan D'Avella <ddavella@stsci.edu>
2017-08-29 14:37:53 -0400

    Customize the pytest id used for reference file tests

--- asdf/tests/test_suite.py
+++ asdf/tests/test_suite.py
@@ -13,7 +13,13 @@ from asdf import versioning
 from .helpers import assert_tree_match
 
 
+def get_test_id(reference_file_path):
+    """Helper function to return the informative part of a schema path"""
+    path = os.path.normpath(reference_file_path)
+    return os.path.sep.join(path.split(os.path.sep)[-3:])
+
 def collect_reference_files():
+    """Function used by pytest to collect ASDF reference files for testing."""
     root = os.path.join(os.path.dirname(__file__), '..', "reference_files")
     for version in versioning.supported_versions:
         version_dir = os.path.join(root, str(version))
@@ -25,7 +31,8 @@ def collect_reference_files():
                     if os.path.exists(basename + ".yaml"):
                         yield filepath
 
-@pytest.mark.parametrize('reference_file', collect_reference_files())
+@pytest.mark.parametrize(
+    'reference_file', collect_reference_files(), ids=get_test_id)
 def test_reference_file(reference_file):
     basename = os.path.basename(reference_file)
 
