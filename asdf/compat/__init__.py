ee43d16ac0a2df2ec51596641d3da82f6b26f258
Daniel D'Avella <drdavella@gmail.com>
2017-12-11 17:18:42 -0500

    Remove user collections from compatibility library

--- asdf/compat/__init__.py
+++ asdf/compat/__init__.py
@@ -6,15 +6,6 @@ from __future__ import absolute_import, division, unicode_literals, print_functi
 import six
 
 if six.PY2:
-    from UserDict import UserDict
-    from UserList import UserList
-    from UserString import UserString
-elif six.PY3:
-    from .user_collections_py3.UserDict import UserDict
-    from .user_collections_py3.UserList import UserList
-    from .user_collections_py3.UserString import UserString
-
-if six.PY2:
     from .functools_backport import lru_cache
 elif six.PY3:
     try:
