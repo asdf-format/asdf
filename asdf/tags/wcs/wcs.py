8e84d2cfdeb683c6e05ebdbbcb8da467f7088e84
Stuart Mumford <stuart@cadair.com>
2018-01-15 16:24:50 +0000

    FrameType now just saves astropy frames and add TemporalFrame

--- asdf/tags/wcs/wcs.py
+++ asdf/tags/wcs/wcs.py
@@ -65,106 +65,9 @@ class StepType(dict, AsdfType):
 
 class FrameType(AsdfType):
     name = "wcs/frame"
-    requires = ['gwcs', 'astropy-1.3.3']
+    requires = ['gwcs']
     types = ['gwcs.Frame2D']
     version = '1.2.0'
-
-    import astropy
-    _astropy_version = astropy.__version__
-    # This indicates that Cartesian Differential is not available
-    _old_astropy = astropy.__version__ <= '1.3.3'
-
-    @classmethod
-    def _get_reference_frame_mapping(cls):
-        if hasattr(cls, '_reference_frame_mapping'):
-            return cls._reference_frame_mapping
-
-        from astropy.coordinates import builtin_frames
-
-        cls._reference_frame_mapping = {
-            'ICRS': builtin_frames.ICRS,
-            'FK5': builtin_frames.FK5,
-            'FK4': builtin_frames.FK4,
-            'FK4_noeterms': builtin_frames.FK4NoETerms,
-            'galactic': builtin_frames.Galactic,
-            'galactocentric': builtin_frames.Galactocentric,
-            'GCRS': builtin_frames.GCRS,
-            'CIRS': builtin_frames.CIRS,
-            'ITRS': builtin_frames.ITRS,
-            'precessed_geocentric': builtin_frames.PrecessedGeocentric
-        }
-
-        return cls._reference_frame_mapping
-
-    @classmethod
-    def _get_inverse_reference_frame_mapping(cls):
-        if hasattr(cls, '_inverse_reference_frame_mapping'):
-            return cls._inverse_reference_frame_mapping
-
-        reference_frame_mapping = cls._get_reference_frame_mapping()
-
-        cls._inverse_reference_frame_mapping = {}
-        for key, val in reference_frame_mapping.items():
-            cls._inverse_reference_frame_mapping[val] = key
-
-        return cls._inverse_reference_frame_mapping
-
-    @classmethod
-    def _reference_frame_from_tree(cls, node, ctx):
-        from astropy.units import Quantity
-        from astropy.io.misc.asdf.tags.unit.quantity import QuantityType
-        from astropy.coordinates import ICRS, CartesianRepresentation
-
-        version = cls.version
-        reference_frame = node['reference_frame']
-        reference_frame_name = reference_frame['type']
-
-        frame_cls = cls._get_reference_frame_mapping()[reference_frame_name]
-
-        frame_kwargs = {}
-        for name in frame_cls.get_frame_attr_names().keys():
-            val = reference_frame.get(name)
-            if val is not None:
-                # These are deprecated fields that must be handled as a special
-                # case for older versions of the schema
-                if name in ['galcen_ra', 'galcen_dec']:
-                    continue
-                # There was no schema for quantities in v1.0.0
-                if name in ['galcen_distance', 'roll', 'z_sun'] and version == '1.0.0':
-                    val = Quantity(val[0], unit=val[1])
-                # These fields are known to be CartesianRepresentations
-                if name in ['obsgeoloc', 'obsgeovel']:
-                    if version == '1.0.0':
-                        unit = val[1]
-                        x = Quantity(val[0][0], unit=unit)
-                        y = Quantity(val[0][1], unit=unit)
-                        z = Quantity(val[0][2], unit=unit)
-                    else:
-                        x = QuantityType.from_tree(val[0], ctx)
-                        y = QuantityType.from_tree(val[1], ctx)
-                        z = QuantityType.from_tree(val[2], ctx)
-                    val = CartesianRepresentation(x, y, z)
-                elif not cls._old_astropy and name == 'galcen_v_sun':
-                    from astropy.coordinates import CartesianDifferential
-                    # This field only exists since v1.1.0, and it only uses
-                    # CartesianDifferential after v1.3.3
-                    d_x = QuantityType.from_tree(val[0], ctx)
-                    d_y = QuantityType.from_tree(val[1], ctx)
-                    d_z = QuantityType.from_tree(val[2], ctx)
-                    val = CartesianDifferential(d_x, d_y, d_z)
-                else:
-                    val = yamlutil.tagged_tree_to_custom_tree(val, ctx)
-                frame_kwargs[name] = val
-        has_ra_and_dec = reference_frame.get('galcen_dec') and \
-            reference_frame.get('galcen_ra')
-        if version == '1.0.0' and has_ra_and_dec:
-            # Convert deprecated ra and dec fields into galcen_coord
-            galcen_dec = reference_frame['galcen_dec']
-            galcen_ra = reference_frame['galcen_ra']
-            dec = Quantity(galcen_dec[0], unit=galcen_dec[1])
-            ra = Quantity(galcen_ra[0], unit=galcen_ra[1])
-            frame_kwargs['galcen_coord'] = ICRS(dec=dec, ra=ra)
-        return frame_cls(**frame_kwargs)
 
     @classmethod
     def _from_tree(cls, node, ctx):
@@ -174,8 +77,7 @@ class FrameType(AsdfType):
             kwargs['axes_names'] = node['axes_names']
 
         if 'reference_frame' in node:
-            kwargs['reference_frame'] = \
-                cls._reference_frame_from_tree(node, ctx)
+            kwargs['reference_frame'] = yamlutil.tagged_tree_to_custom_tree(node['reference_frame'], ctx)
 
         if 'axes_order' in node:
             kwargs['axes_order'] = tuple(node['axes_order'])
@@ -188,11 +90,6 @@ class FrameType(AsdfType):
 
     @classmethod
     def _to_tree(cls, frame, ctx):
-        import numpy as np
-        from astropy.coordinates import CartesianRepresentation
-        from astropy.io.misc.asdf.tags.unit.quantity import QuantityType
-        if not cls._old_astropy:
-            from astropy.coordinates import CartesianDifferential
 
         node = {}
 
@@ -205,24 +102,7 @@ class FrameType(AsdfType):
             node['axes_names'] = list(frame.axes_names)
 
         if frame.reference_frame is not None:
-            reference_frame = {}
-            reference_frame['type'] = cls._get_inverse_reference_frame_mapping()[
-                type(frame.reference_frame)]
-
-            for name in frame.reference_frame.get_frame_attr_names().keys():
-                frameval = getattr(frame.reference_frame, name)
-                # CartesianRepresentation becomes a flat list of x,y,z
-                # coordinates with associated units
-                if isinstance(frameval, CartesianRepresentation):
-                    value = [frameval.x, frameval.y, frameval.z]
-                    frameval = value
-                elif not cls._old_astropy and isinstance(frameval, CartesianDifferential):
-                    value = [frameval.d_x, frameval.d_y, frameval.d_z]
-                    frameval = value
-                yamlval = yamlutil.custom_tree_to_tagged_tree(frameval, ctx)
-                reference_frame[name] = yamlval
-
-            node['reference_frame'] = reference_frame
+            node['reference_frame'] = yamlutil.custom_tree_to_tagged_tree(frame.reference_frame, ctx)
 
         if frame.unit is not None:
             node['unit'] = yamlutil.custom_tree_to_tagged_tree(
@@ -237,7 +117,7 @@ class FrameType(AsdfType):
         assert old.name == new.name
         assert old.axes_order == new.axes_order
         assert old.axes_names == new.axes_names
-        assert type(old.reference_frame) == type(new.reference_frame)
+        assert type(old.reference_frame) is type(new.reference_frame)
         assert old.unit == new.unit
 
         if old.reference_frame is not None:
@@ -311,6 +191,56 @@ class SpectralFrame(FrameType):
         return node
 
 
+class TemporalFrame(AsdfType):
+    name = "wcs/temporal_frame"
+    requires = ['gwcs']
+    types = ['gwcs.coordinate_frames.TemporalFrame']
+    version = '1.2.0'
+
+    @classmethod
+    def to_tree(cls, frame, ctx):
+        import astropy.time
+
+        node = {}
+
+        node['name'] = frame.name
+
+        if frame.axes_order != (0, 1):
+            node['axes_order'] = list(frame.axes_order)
+
+        if frame.axes_names is not None:
+            node['axes_names'] = list(frame.axes_names)
+
+        if frame.reference_frame is not None:
+            if frame.reference_frame is not astropy.time.Time:
+                raise ValueError("Can not save reference_frame unless it's Time")
+
+        if frame.reference_position is not None:
+            node['reference_time'] = yamlutil.custom_tree_to_tagged_tree(
+                frame.reference_position, ctx)
+
+        if frame.unit is not None:
+            node['unit'] = yamlutil.custom_tree_to_tagged_tree(
+                list(frame.unit), ctx)
+
+        return node
+
+    @classmethod
+    def from_tree(cls, node, ctx):
+        from gwcs.coordinate_frames import TemporalFrame as gTemporalFrame
+        import astropy.time
+
+        name = node['name']
+        axes_order = node.get('axes_order', None)
+        axes_names = node.get('axes_names', None)
+        reference_frame = node.get('reference_frame', astropy.time.Time)
+        reference_time = node.get('reference_time', None)
+        unit = node.get('unit', None)
+
+        return gTemporalFrame(axes_order, reference_time,
+                              reference_frame, unit, axes_names, name)
+
+
 class CompositeFrame(FrameType):
     name = "wcs/composite_frame"
     types = ['gwcs.CompositeFrame']
@@ -343,54 +273,54 @@ class CompositeFrame(FrameType):
         for old_frame, new_frame in zip(old.frames, new.frames):
             helpers.assert_tree_match(old_frame, new_frame)
 
-class ICRSCoord(AsdfType):
-    """The newest version of this tag and the associated schema have  moved to
-    Astropy. This implementation is retained here for the purposes of backwards
-    compatibility with older files.
-    """
-    name = "wcs/icrs_coord"
-    types = ['astropy.coordinates.ICRS']
-    requires = ['astropy']
-    version = "1.1.0"
+# class ICRSCoord(AsdfType):
+#     """The newest version of this tag and the associated schema have  moved to
+#     Astropy. This implementation is retained here for the purposes of backwards
+#     compatibility with older files.
+#     """
+#     name = "wcs/icrs_coord"
+#     types = ['astropy.coordinates.ICRS']
+#     requires = ['astropy']
+#     version = "1.1.0"
 
-    @classmethod
-    def from_tree(cls, node, ctx):
-        from astropy.io.misc.asdf.tags.unit.quantity import QuantityType
-        from astropy.coordinates import ICRS, Longitude, Latitude, Angle
+#     @classmethod
+#     def from_tree(cls, node, ctx):
+#         from astropy.io.misc.asdf.tags.unit.quantity import QuantityType
+#         from astropy.coordinates import ICRS, Longitude, Latitude, Angle
 
-        angle = QuantityType.from_tree(node['ra']['wrap_angle'], ctx)
-        wrap_angle = Angle(angle.value, unit=angle.unit)
-        ra = Longitude(
-            node['ra']['value'],
-            unit=node['ra']['unit'],
-            wrap_angle=wrap_angle)
-        dec = Latitude(node['dec']['value'], unit=node['dec']['unit'])
+#         angle = QuantityType.from_tree(node['ra']['wrap_angle'], ctx)
+#         wrap_angle = Angle(angle.value, unit=angle.unit)
+#         ra = Longitude(
+#             node['ra']['value'],
+#             unit=node['ra']['unit'],
+#             wrap_angle=wrap_angle)
+#         dec = Latitude(node['dec']['value'], unit=node['dec']['unit'])
 
-        return ICRS(ra=ra, dec=dec)
+#         return ICRS(ra=ra, dec=dec)
 
-    @classmethod
-    def to_tree(cls, frame, ctx): # pragma: no cover
-        # We do not run coverage analysis since new ICRS objects will be
-        # serialized by the tag implementation in Astropy. Eventually if we
-        # have a better way to write older versions of tags, we can re-add
-        # tests for this code.
-        from astropy.units import Quantity
-        from astropy.coordinates import ICRS
-        from astropy.io.misc.asdf.tags.unit.quantity import QuantityType
+#     @classmethod
+#     def to_tree(cls, frame, ctx): # pragma: no cover
+#         # We do not run coverage analysis since new ICRS objects will be
+#         # serialized by the tag implementation in Astropy. Eventually if we
+#         # have a better way to write older versions of tags, we can re-add
+#         # tests for this code.
+#         from astropy.units import Quantity
+#         from astropy.coordinates import ICRS
+#         from astropy.io.misc.asdf.tags.unit.quantity import QuantityType
 
-        node = {}
+#         node = {}
 
-        wrap_angle = Quantity(
-            frame.ra.wrap_angle.value,
-            unit=frame.ra.wrap_angle.unit)
-        node['ra'] = {
-            'value': frame.ra.value,
-            'unit': frame.ra.unit.to_string(),
-            'wrap_angle': yamlutil.custom_tree_to_tagged_tree(wrap_angle, ctx)
-        }
-        node['dec'] = {
-            'value': frame.dec.value,
-            'unit': frame.dec.unit.to_string()
-        }
+#         wrap_angle = Quantity(
+#             frame.ra.wrap_angle.value,
+#             unit=frame.ra.wrap_angle.unit)
+#         node['ra'] = {
+#             'value': frame.ra.value,
+#             'unit': frame.ra.unit.to_string(),
+#             'wrap_angle': yamlutil.custom_tree_to_tagged_tree(wrap_angle, ctx)
+#         }
+#         node['dec'] = {
+#             'value': frame.dec.value,
+#             'unit': frame.dec.unit.to_string()
+#         }
 
-        return node
+#         return node
