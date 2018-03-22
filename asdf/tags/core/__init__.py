1d582c28f0143f85c2f9a2ad69bc71f934196f17
Daniel D'Avella <ddavella@stsci.edu>
2018-03-22 17:47:01 -0400

    Implement tag for ExtensionMetadata. Update asdf-standard

--- asdf/tags/core/__init__.py
+++ asdf/tags/core/__init__.py
@@ -3,6 +3,7 @@
 
 
 from ...asdftypes import AsdfType
+from ...yamlutil import custom_tree_to_tagged_tree
 
 
 class AsdfObject(dict, AsdfType):
@@ -12,10 +13,32 @@ class AsdfObject(dict, AsdfType):
 
 class Software(dict, AsdfType):
     name = 'core/software'
+    version = '1.0.0'
 
 
 class HistoryEntry(dict, AsdfType):
     name = 'core/history_entry'
+    version = '1.0.0'
+
+
+class ExtensionMetadata(AsdfType):
+    name = 'core/extension_metadata'
+    version = '1.0.0'
+
+    def __init__(self, extension_class=None, software={}):
+        self.extension_class = extension_class
+        self.software = software
+
+    @classmethod
+    def from_tree(cls, node, ctx):
+        return cls(**node)
+
+    @classmethod
+    def to_tree(cls, node, ctx):
+        tree = {}
+        tree['extension_class'] = node.extension_class
+        tree['software'] = custom_tree_to_tagged_tree(node.software, ctx)
+        return tree
 
 
 from .constant import ConstantType
