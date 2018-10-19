9c6454bbea91d05ffb65b283da4824ad7c5365c9
Daniel D'Avella <ddavella@stsci.edu>
2018-10-19 16:42:06 -0400

    Move pytest_plugins to top-level conftest per pytest deprecation

--- asdf/conftest.py
+++ asdf/conftest.py
@@ -21,11 +21,6 @@ packagename = os.path.basename(os.path.dirname(__file__))
 TESTED_VERSIONS[packagename] = version.version
 
 
-pytest_plugins = [
-    'asdf.tests.schema_tester'
-]
-
-
 try:
     PYTEST_HEADER_MODULES['Astropy'] = 'astropy'
     PYTEST_HEADER_MODULES['jsonschema'] = 'jsonschema'
