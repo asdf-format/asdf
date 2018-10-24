78d397c6a6f04c0a0aece775e63606d417617182
Vadim Markovtsev <vadim@sourced.tech>
2018-10-24 17:13:36 +0200

    Ensure that all the blocks are fully read if lazy_load=False

--- asdf/tests/helpers.py
+++ asdf/tests/helpers.py
@@ -20,9 +20,9 @@ except ImportError:
     CartesianDifferential = None
 
 from ..asdf import AsdfFile, get_asdf_library_info
+from ..block import Block
 from .httpserver import RangeHTTPServer
 from ..extension import default_extensions
-from .. import util
 from .. import versioning
 
 from ..tags.core import AsdfObject
@@ -235,6 +235,10 @@ def assert_roundtrip_tree(tree, tmpdir, *, asdf_check_func=None,
         AsdfFile(tree, extensions=extensions, **init_options).write_to(buff, **write_options)
         buff.seek(0)
         ff = AsdfFile.open(buff, extensions=extensions, copy_arrays=True, lazy_load=False)
+        # Ensure that all the blocks are loaded
+        for block in ff.blocks._internal_blocks:
+            assert isinstance(block, Block)
+            assert block._data is not None
     # The underlying file is closed at this time and everything should still work
     assert_tree_match(tree, ff.tree, ff, funcname=tree_match_func)
     if asdf_check_func:
@@ -244,6 +248,9 @@ def assert_roundtrip_tree(tree, tmpdir, *, asdf_check_func=None,
     AsdfFile(tree, extensions=extensions, **init_options).write_to(fname, **write_options)
     with AsdfFile.open(fname, mode='rw', extensions=extensions, copy_arrays=False,
                        lazy_load=False) as ff:
+        for block in ff.blocks._internal_blocks:
+            assert isinstance(block, Block)
+            assert block._data is not None
         assert_tree_match(tree, ff.tree, ff, funcname=tree_match_func)
         if asdf_check_func:
             asdf_check_func(ff)
