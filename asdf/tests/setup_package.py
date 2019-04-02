9668f419a7bf2ef79871aa5b9988da1d1e91e984
Daniel D'Avella <ddavella@stsci.edu>
2019-04-02 15:21:03 -0400

    Remove setup_package.py files. They are no longer used

--- asdf/tests/setup_package.py
+++
@@ -1,9 +0,0 @@
-# Licensed under a 3-clause BSD style license - see LICENSE.rst
-# -*- coding: utf-8 -*-
-
-
-
-def get_package_data():  # pragma: no cover
-    return {
-        str(_PACKAGE_NAME_ + '.tests'):
-            ['coveragerc', 'data/*.yaml', 'data/*.json', 'data/*.fits', 'data/*.fits.gz']}
