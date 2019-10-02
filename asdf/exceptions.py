30e5f6ae735a5ae937cccb44e5235d5c157b5f1a
James Davies <jdavies@stsci.edu>
2019-10-02 13:40:06 -0400

    AsdfDeprecationWarning is subclass of DeprecationWarning

--- asdf/exceptions.py
+++ asdf/exceptions.py
@@ -7,7 +7,7 @@ class AsdfWarning(Warning):
     The base warning class from which all ASDF warnings should inherit.
     """
 
-class AsdfDeprecationWarning(AsdfWarning):
+class AsdfDeprecationWarning(AsdfWarning, DeprecationWarning):
     """
     A warning class to indicate a deprecated feature.
     """
