6f0d543cc915445ec1c5536e18d9d1d10f51fec2
Ken MacDonald <kmacdonald@stsci.edu>
2020-09-22 17:24:57 -0400

    Reformatting the tests and changing some values in the tree.

--- asdf/commands/tests/test_edit.py
+++ asdf/commands/tests/test_edit.py
@@ -61,10 +61,10 @@ def test_edit_smaller(tmpdir, version):
     args = ["edit", "-s", "-f", f"{yaml_edit}", "-o", f"{asdf_edit}"]
     ret = main.main_from_args(args)
     assert 0==ret
+    assert os.path.getsize(asdf_edit) == os.path.getsize(asdf_base)
 
     with asdf.open(asdf_edit) as af:
         assert af.tree["foo"] == 2
-        assert os.path.getsize(asdf_edit) == os.path.getsize(asdf_base)
 
 @pytest.mark.parametrize("version", asdf.versioning.supported_versions)
 def test_edit_equal(tmpdir, version):
@@ -75,21 +75,21 @@ def test_edit_equal(tmpdir, version):
     args = ["edit", "-s", "-f", f"{yaml_edit}", "-o", f"{asdf_edit}"]
     ret = main.main_from_args(args)
     assert 0==ret
+    assert os.path.getsize(asdf_edit) == os.path.getsize(asdf_base)
 
     with asdf.open(asdf_edit) as af:
         assert af.tree["foo"] == 41
-        assert os.path.getsize(asdf_edit) == os.path.getsize(asdf_base)
 
 @pytest.mark.parametrize("version", asdf.versioning.supported_versions)
 def test_edit_larger(tmpdir, version):
     asdf_base, yaml_base, asdf_edit, yaml_edit = _initialize_test(tmpdir, version)
 
-    _create_edited_yaml(yaml_base, yaml_edit, "foo: 42", "foo: 41\nbar: 13")
+    _create_edited_yaml(yaml_base, yaml_edit, "foo: 42", "foo: 42\nbar: 13")
 
     args = ["edit", "-s", "-f", f"{yaml_edit}", "-o", f"{asdf_edit}"]
     ret = main.main_from_args(args)
     assert 0==ret
+    assert os.path.getsize(asdf_edit) - os.path.getsize(asdf_base) > 10000
 
     with asdf.open(asdf_edit) as af:
         assert "bar" in af.tree
-        assert os.path.getsize(asdf_edit) - os.path.getsize(asdf_base) > 10000
