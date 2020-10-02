0102261e35ff466814de243306bd6430ac0c58ff
Ken MacDonald <kmacdonald@stsci.edu>
2020-10-02 12:57:08 -0400

    Incorporating PR feedback and correctly implementing padding.  The cases for edited YAML portions smaller or equal to the existing YAML in an ASDF file are complete.

--- asdf/commands/edit.py
+++ asdf/commands/edit.py
@@ -35,7 +35,7 @@ class Edit(Command):
 
         # Set up the parser
         parser = subparsers.add_parser(
-            str("edit"),
+            "edit",
             help="Edit YAML portion of an ASDF file.",
             description=desc_string,
         )
@@ -59,6 +59,18 @@ class Edit(Command):
             dest="oname",
             help="Output file (YAML for -e option, ASDF for -s option",
         )
+
+        """
+        # Validate input YAML.  Optional, since this could be what's being corrected.
+        parser.add_argument(
+            "--validate",
+            "-v",
+            type=bool,
+            action="store_true",
+            dest="validate",
+            help="Validate input YAML format.",
+        )
+        """
 
         # The edit is either being performed or saved
         group = parser.add_mutually_exclusive_group(required=True)
@@ -105,7 +117,7 @@ def is_yaml_file(fname):
     """
 
     base, ext = os.path.splitext(fname)
-    if ".yaml" != ext:
+    if ".yaml" != ext and ".yml" != ext:
         return False
     return True
 
@@ -173,7 +185,7 @@ def is_valid_yaml_path(fname):
     ------
     bool
     """
-    ext = [".yaml"]
+    ext = [".yaml", ".yml"]
     if is_valid_path_and_ext(fname, ext):
         return True
     print(f"Error: '{fname}' should have extension '{ext[0]}'")
@@ -233,7 +245,7 @@ def open_and_check_asdf_header(fname):
     return fd, header_and_comment  # Return GenericFile and ASDF header bytes.
 
 
-def read_and_validate_yaml(fd, fname):
+def read_and_validate_yaml(fd, fname, validate_yaml):
     """
     Get the YAML text from an ASDF formatted file.
 
@@ -263,14 +275,15 @@ def read_and_validate_yaml(fd, fname):
     )
     yaml_content = reader.read()
 
-    # Create a YAML tree to validate
-    # The YAML text must be converted to a stream.
-    tree = yamlutil.load_tree(io.BytesIO(yaml_content))
-    if tree is None:
-        print("Error: 'yamlutil.load_tree' failed to return a tree.")
-        sys.exist(1)
+    if validate_yaml:
+        # Create a YAML tree to validate
+        # The YAML text must be converted to a stream.
+        tree = yamlutil.load_tree(io.BytesIO(yaml_content))
+        if tree is None:
+            print("Error: 'yamlutil.load_tree' failed to return a tree.")
+            sys.exist(1)
 
-    schema.validate(tree, None)  # Failure raises an exception.
+        schema.validate(tree, None)  # Failure raises an exception.
 
     return yaml_content
 
@@ -291,17 +304,16 @@ def edit_func(fname, oname):
     if not is_valid_asdf_path(fname):
         return False
 
+    if not is_yaml_file(oname):
+        print("A YAML file is expected, with '.yaml' or '.yml' extension.")
+        sys.exit(1)
+
     # Validate input file is an ASDF file.
     fd, asdf_text = open_and_check_asdf_header(fname)
 
     # Read and validate the YAML of an ASDF file.
-    yaml_text = read_and_validate_yaml(fd, fname)
+    yaml_text = read_and_validate_yaml(fd, fname, False)
     fd.close()
-
-    # Open a YAML file for the ASDF YAML.
-    if not is_yaml_file(oname):
-        print("A YAML file is expected, with '.yaml' extension.")
-        sys.exit(1)
 
     # Write the YAML for the original ASDF file.
     with open(oname, "wb") as ofd:
@@ -320,71 +332,6 @@ def edit_func(fname, oname):
     print(f"{delim}\n")
 
     return
-
-
-def get_yaml_text_and_delimiter(yaml_text):
-    """
-    Splits the YAML text into the text and the end delimiter in preparation
-    for padding.
-    """
-
-    wdelim = b"\r\n...\r\n"
-    ldelim = b"\n...\n"
-    if yaml_text[-len(wdelim) :] == wdelim:
-        delim = wdelim
-    elif yaml_text[-len(ldelim) :] == ldelim:
-        delim = ldelim
-    else:
-        print("Unrecognized YAML delimiter ending the YAML text.")
-        print(f"It should be {wdelim} or {ldelim}, but the")
-        print(f"last {len(wdelim)} bytes are {yaml_text[-len(wdelim):]}.")
-        sys.exit(1)
-
-    return yaml_text[: -len(delim)], delim
-
-
-def pad_edited_text(edited_text, orig_text):
-    """
-    There is more text in the original ASDF file than in the edited text,
-    so we will pad the edited text with spaces.
-
-    Parameters
-    ----------
-    edited_text - The text from the edited YAML file
-    orig_text - The text from the original ASDF file
-
-    Return
-    ------
-    The padded text and the number of spaces added as pad.
-    """
-    diff = len(orig_text) - len(edited_text)
-
-    edited_text, delim = get_yaml_text_and_delimiter(edited_text)
-
-    padded_text = edited_text + b"\n" + b" " * (diff - 1) + delim
-    return padded_text, diff - 1
-
-
-def add_pad_to_new_text(edited_text, pad_size):
-    """
-    Adds pad to edited text.
-
-    Parameters
-    ----------
-    edited_text - The text from the edited YAML file.
-    pad_size - The number of spaces to add as a pad.
-
-    Return
-    ------
-    Pad text with the number of spaces requested as pad.
-    """
-
-    edited_text, delim = get_yaml_text_and_delimiter(edited_text)
-
-    pad = b" " * pad_size
-    padded_text = edited_text + b"\n" + pad + delim
-
-    return padded_text
 
 
 def write_block_index(fd, index):
@@ -410,106 +357,46 @@ def write_block_index(fd, index):
     return
 
 
-def get_next_block_header(fd):
+def find_first_block(fname):
     """
-    From a file, gets the next block header.
+    Finds the location of the first binary block in an ASDF file.
+
 
     Parameters
     ----------
-    fd - The ASDF file to get the next block.
+    fname : str
+        Input ASDF file name.
 
     Return
     ------
-    If a block is found, return the bytes of the block header.
-    Otherwise return None.
+    Location, in bytes, of the first binary block.
     """
-    #     Block header structure:
-    # 4 bytes of magic number
-    # 2 bytes of header length, after the length field (min 48)
-    # 4 bytes flag
-    # 4 bytes compression
-    # 8 bytes allocated size
-    # 8 bytes used (on disk) size
-    # 8 bytes data size
-    # 16 bytes checksum
-    blk_header = fd.read(6)
-    if len(blk_header) != 6:
-        return None
-    if not blk_header.startswith(constants.BLOCK_MAGIC):
-        return None
-    hsz = struct.unpack(">H", blk_header[4:6])[0]
-    header = fd.read(hsz)
-    return blk_header + header
+    with generic_io.get_file(fname, mode="r") as fd:
+        reader = fd.reader_until(
+            constants.BLOCK_MAGIC,
+            7,
+            "First binary block",
+            include=False,
+        )
+        content_to_first_block = reader.read()
+    return len(content_to_first_block) 
+
+def write_edited_yaml_larger (oname, edited_yaml, first_block_loc):
+    print("Larger")
 
 
-def rewrite_asdf_file(edited_text, orig_text, oname, fname):
-    """
-    Rewrite an ASDF file for too large edited YAML.  The edited YAML, a pad,
-    the blocks will be rewritten.  A block index will also be rewritten.  If a
-    block index existed in the old file, it will have to be recomputed to
-    because of the larger YAML size and pad, which changes the location of
-    the binary blocks.
-
-    Parameters
-    ----------
-    edited_text : the new YAML text to write out.
-    orig_text : the original YAML text to overwrite.
-    oname : the ASDF file to overwrite.
-    fname : the edit YAML to write to new file.
-    """
-
-    tmp_oname = oname + ".tmp"  # Save as a temp file, in case anything goes wrong.
-    pad_size = 10 * 1000
-    padded_text = add_pad_to_new_text(edited_text, pad_size)
-
-    ifd = open(oname, "r+b")  # Open old ASDF to get binary blocks
-    ifd.seek(len(orig_text))
-
-    ofd = open(tmp_oname, "w+b")  # Open temp file to write
-    ofd.write(padded_text)  # Write edited YAML
-
-    current_location = len(padded_text)
-    block_index = []
-    alloc_loc = 14  # 4 bytes of block ID, 2 blocks of size, 8 blocks into header
-    block_chunk = 2048
-    while True:
-        next_block = get_next_block_header(ifd)
-        if next_block is None:
-            break
-
-        # Get block size on disk
-        alloc = struct.unpack(">Q", next_block[alloc_loc : alloc_loc + 8])[0]
-
-        # Save block location for block index
-        block_index.append(current_location)
-        current_location = current_location + len(next_block) + alloc
-
-        # Copy block
-        ofd.write(next_block)
-        while alloc >= block_chunk:
-            chunk = ifd.read(block_chunk)
-            ofd.write(chunk)
-            alloc -= block_chunk
-        if alloc > 0:
-            chunk = ifd.read(alloc)
-            ofd.write(chunk)
-    ifd.close()
-
-    write_block_index(ofd, block_index)
-    ofd.close()
-
-    # Rename temp file.
-    os.replace(tmp_oname, oname)
-
-    # Output message to user.
-    delim = "*" * 70
-    print(f"\n{delim}")
-    print(f"The text in '{fname}' was too large to simply overwrite the")
-    print(f"text in '{oname}'.  The file '{oname}' was rewritten to")
-    print("accommodate the larger text size.")
-    print(f"Also, added a '\\n' and {pad_size:,} ' ' as a pad for")
-    print(f"the text in '{oname}' to allow for future edits.")
-    print(f"{delim}\n")
+def write_edited_yaml(oname, edited_yaml, first_block_loc):
+    if len(edited_yaml) < first_block_loc:
+        pad_length = first_block_loc - len(edited_yaml)
+        padding = b'\0' * pad_length
+        with open(oname, "r+b") as fd:
+            fd.write(edited_yaml)
+            fd.write(padding)
+    elif len(edited_yaml) == first_block_loc:
+        with open(oname, "r+b") as fd:
+            fd.write(edited_yaml)
+    else:
+        write_edited_yaml_larger(oname, edited_yaml, first_block_loc)  
 
 
 def save_func(fname, oname):
@@ -525,8 +412,10 @@ def save_func(fname, oname):
 
     Parameters
     ----------
-    fname : The input YAML file.
-    oname : The output ASDF file name.
+    fname : str
+        Input YAML file name.
+    oname : str
+        The output ASDF file name.
     """
 
     if not is_valid_yaml_path(fname):
@@ -537,37 +426,25 @@ def save_func(fname, oname):
 
     # Validate input file is an ASDF formatted YAML.
     ifd, iasdf_text = open_and_check_asdf_header(fname)
-    iyaml_text = read_and_validate_yaml(ifd, fname)
+    iyaml_text = read_and_validate_yaml(ifd, fname, True)
     ifd.close()
     edited_text = iasdf_text + iyaml_text
 
-    # Get text from ASDF file.
-    ofd, oasdf_text = open_and_check_asdf_header(oname)
-    oyaml_text = read_and_validate_yaml(ofd, oname)
-    ofd.close()
-    asdf_text = oasdf_text + oyaml_text
-
-    # Compare text sizes and maybe output.
-    # There are three cases:
-    msg_delim = "*" * 70
-    if len(edited_text) == len(asdf_text):
-        with open(oname, "r+b") as fd:
-            fd.write(edited_text)
-        print(f"\n{msg_delim}")
-        print(f"The edited text in '{fname}' was written to '{oname}'")
-        print(f"{msg_delim}\n")
-    elif len(edited_text) < len(asdf_text):
-        padded_text, diff = pad_edited_text(edited_text, asdf_text)
-        with open(oname, "r+b") as fd:
-            fd.write(padded_text)
-        print(f"\n{msg_delim}")
-        print(f"The edited text in '{fname}' was written to '{oname}'")
-        print(
-            f"Added a '\\n' and {diff} pad of ' ' between the YAML text and binary blocks."
-        )
-        print(f"{msg_delim}\n")
-    else:
-        rewrite_asdf_file(edited_text, asdf_text, oname, fname)
+    # - Find location of first block.
+    loc = find_first_block(oname)
+    write_edited_yaml(oname, edited_text, loc)
+    # - Using edited_text length and first block location determine.
+    #   space availability for new YAML.
+    # - Smaller:
+    #   - Overwrite YAML in ASDF, then pad 0x00 to first b'\xd3BLK'.
+    # - Equal:
+    #   - Overwrite YAML in ASDF.  No padding.
+    # - Larger:
+    #   - Create new file.
+    #   - Write new YAML.
+    #   - Add 10,000 0x00 pad bytes between '...\r?\n' and '\xd3BLK'.
+    #   - If no streaming block found, write block index.
+    #   - If streaming block found, handle correctly.
 
     return
 
