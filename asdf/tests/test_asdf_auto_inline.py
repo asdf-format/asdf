15680760145a52637c43bfdfc5980bad84bd6fb0
Ken MacDonald <kmacdonald@stsci.edu>
2020-10-12 15:48:22 -0400

    Addig auto_inline default testing.

---
+++ asdf/tests/test_asdf_auto_inline.py
@@ -0,0 +1,31 @@
+import numpy as np
+import os
+import pytest
+
+import asdf
+
+
+def _write_test_file(version, oname):
+    small_array = np.random.rand(75)
+    large_array = np.random.rand(150)
+    tree = {
+        "author": "Monty", 
+        "foo": 42, 
+        "bar": 13,
+        "small_array": small_array,
+        "large_array": large_array
+    }
+    asdf_name = os.path.join(oname)
+    with asdf.AsdfFile(tree, version=version) as af:
+        af.write_to(asdf_name)
+
+
+@pytest.mark.parametrize("version", asdf.versioning.supported_versions)
+def test_write_to_auto_inline(tmpdir, version):
+    fname = "auto_inline_test.asdf"
+    oname = os.path.join(tmpdir, fname)
+    _write_test_file(version, oname)
+
+    with asdf.open(oname) as af:
+        assert len(af._blocks._internal_blocks) == 1
+        assert len(af._blocks._inline_blocks) == 1
