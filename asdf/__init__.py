a148b907d441917863908e71384fd88b91af3eed
Ed Slavich <eslavich@stsci.edu>
2021-03-22 10:34:38 -0400

    Remove defunct asdf.test method and use setuptools-scm to generate version file

--- asdf/__init__.py
+++ asdf/__init__.py
@@ -3,16 +3,10 @@ asdf: Python library for reading and writing Advanced Scientific
 Data Format (ASDF) files
 """
 
-# Affiliated packages may add whatever they like to this file, but
-# should keep this content at the top.
-# ----------------------------------------------------------------------------
-from ._internal_init import __version__, __githash__, test
-# ----------------------------------------------------------------------------
-
 __all__ = [
-    'AsdfFile', 'CustomType', 'AsdfExtension', 'Stream', 'open', 'test',
+    'AsdfFile', 'CustomType', 'AsdfExtension', 'Stream', 'open',
     'commands', 'IntegerType', 'ExternalArrayReference', 'info', '__version__',
-    '__githash__', 'ValidationError', 'get_config', 'config_context',
+    'ValidationError', 'get_config', 'config_context',
 ]
 
 
@@ -25,5 +19,6 @@ from .tags.core import IntegerType
 from .tags.core.external_reference import ExternalArrayReference
 from ._convenience import info
 from .config import get_config, config_context
+from .version import version as __version__
 
 from jsonschema import ValidationError
