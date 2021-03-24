6b22589a9454bd4bfeb64e922eab0e7ddb4e9e3c
Ed Slavich <eslavich@stsci.edu>
2021-03-24 11:36:12 -0400

    Change out of temporary directory before deleting it

--- conftest.py
+++ conftest.py
@@ -7,9 +7,9 @@ import pytest
 @pytest.fixture(scope="session", autouse=True)
 def temp_cwd():
     original_cwd = os.getcwd()
-    try:
-        with tempfile.TemporaryDirectory() as tmp:
+    with tempfile.TemporaryDirectory() as tmp:
+        try:
             os.chdir(tmp)
             yield
-    finally:
-        os.chdir(original_cwd)
+        finally:
+            os.chdir(original_cwd)
