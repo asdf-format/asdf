7152b84304240f6b11c29ec00f5b8ae1b739848b
Ed Slavich <eslavich@stsci.edu>
2021-03-24 11:36:12 -0400

    Change out of temporary directory before deleting it

--- conftest.py
+++ conftest.py
@@ -1,20 +1,12 @@
 import pytest
 
-@pytest.fixture(autouse=True)
-def _docdir(request):
-    """
-    Make sure that doctests run in a temporary directory so that any files that
-    are created as part of the test get removed automatically.
-    """
-    # Trigger ONLY for doctestplus.
-    try:
-        doctest_plugin = request.config.pluginmanager.getplugin("doctestplus")
-        if isinstance(request.node.parent, doctest_plugin._doctest_textfile_item_cls):
-            tmpdir = request.getfixturevalue('tmpdir')
-            with tmpdir.as_cwd():
-                yield
-        else:
+
+@pytest.fixture(scope="session", autouse=True)
+def temp_cwd():
+    original_cwd = os.getcwd()
+    with tempfile.TemporaryDirectory() as tmp:
+        try:
+            os.chdir(tmp)
             yield
-    # Handle case where doctestplus is not available
-    except AttributeError:
-        yield
+        finally:
+            os.chdir(original_cwd)
