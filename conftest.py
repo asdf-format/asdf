5513d6321cdd441edfe310f761feda02b9cf2499
Ed Slavich <eslavich@stsci.edu>
2021-03-24 12:18:59 -0400

    Pytest tmpdir_factory fixture is a better way

--- conftest.py
+++ conftest.py
@@ -1,12 +1,17 @@
+import os
+
 import pytest
 
 
 @pytest.fixture(scope="session", autouse=True)
-def temp_cwd():
+def temp_cwd(tmpdir_factory):
+    """
+    This fixture creates a temporary current working directory
+    for the test session, so that docstring tests that write files
+    don't clutter up the real cwd.
+    """
     original_cwd = os.getcwd()
-    with tempfile.TemporaryDirectory() as tmp:
-        try:
-            os.chdir(tmp)
-            yield
-        finally:
-            os.chdir(original_cwd)
+    try:
+        os.chdir(tmpdir_factory.mktemp("cwd"))
+    finally:
+        os.chdir(original_cwd)
