82852f3f0f1db113bcc87e9371f297c6195aa9f1
Ed Slavich <eslavich@stsci.edu>
2021-05-04 11:31:18 -0400

    Update asdftool tags and asdftool extensions for new extension API

---
+++ asdf/commands/tests/test_extension.py
@@ -0,0 +1,16 @@
+import pytest
+
+from .. import find_extensions
+
+
+@pytest.mark.parametrize("summary", [True, False])
+@pytest.mark.parametrize("tags_only", [True, False])
+def test_parameter_combinations(summary, tags_only):
+    # Just confirming no errors:
+    find_extensions(summary, tags_only)
+
+
+def test_builtin_extension_included(capsys):
+    find_extensions(True, False)
+    captured = capsys.readouterr()
+    assert "asdf.extension.BuiltinExtension" in captured.out
--- asdf/commands/tests/test_tags.py
+++ asdf/commands/tests/test_tags.py
@@ -1,36 +1,25 @@
 import io
 
+import pytest
+
 from ... import AsdfFile
 from .. import list_tags
 
-def _get_tags(display_classes):
+
+@pytest.mark.parametrize("display_classes", [True, False])
+def test_parameter_combinations(display_classes):
+    # Just confirming no errors:
+    list_tags(display_classes)
+
+
+def test_all_tags_present():
     iostream = io.StringIO()
-    list_tags(display_classes=display_classes, iostream=iostream)
+    list_tags(iostream=iostream)
     iostream.seek(0)
-    return [line.strip() for line in iostream.readlines()]
-
-def _class_to_string(_class):
-    return "{}.{}".format(_class.__module__, _class.__name__)
-
-def test_list_schemas():
-    obs_tags = _get_tags(False)
+    tags = {line.strip() for line in iostream.readlines()}
 
     af = AsdfFile()
-    exp_tags = sorted(af.type_index._type_by_tag.keys())
-
-    for exp, obs in zip(exp_tags, obs_tags):
-        assert exp == obs
-
-def test_list_schemas_and_tags():
-    tag_lines = _get_tags(True)
-
-    af = AsdfFile()
-    type_by_tag = af.type_index._type_by_tag
-    exp_tags = sorted(type_by_tag.keys())
-
-    for exp_tag, line in zip(exp_tags, tag_lines):
-        tag_name, tag_class = line.split(":  ")
-        assert tag_name == exp_tag
-
-        exp_class = _class_to_string(type_by_tag[exp_tag])
-        assert tag_class == exp_class
+    for tag in af.type_index._type_by_tag:
+        assert tag in tags
+    for tag in af.extension_manager._tag_defs_by_tag:
+        assert tag in tags
