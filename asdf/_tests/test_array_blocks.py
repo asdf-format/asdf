3f0bef2cc06fc337b84cc2f03d6d747f9e498ae8
Brett <brettgraham@gmail.com>
2023-01-20 16:54:21 -0500

    add data_callback to Block.__init__
    used for providing a function that will later
    (during write) be used to retrieve the data corresponding
    to the block

--- asdf/_tests/test_array_blocks.py
+++ asdf/_tests/test_array_blocks.py
@@ -927,3 +927,47 @@ def test_block_key():
     bm.remove(blk)
     assert arr_blk in bm._internal_blocks
     assert blk not in bm._internal_blocks
+
+
+@pytest.mark.parametrize("memmap", [True, False])
+@pytest.mark.parametrize("lazy_load", [True, False])
+def test_data_callback(tmp_path, memmap, lazy_load):
+    class Callback:
+        def __init__(self, data):
+            self.n_calls = 0
+            self.data = data
+
+        def __call__(self):
+            self.n_calls += 1
+            return self.data
+
+    arr = np.array([1, 2, 3], dtype="uint8")
+    callback = Callback(arr)
+    b = block.Block(memmap=memmap, lazy_load=lazy_load, data_callback=callback)
+
+    assert callback.n_calls == 0
+    assert b.data is arr
+    assert callback.n_calls == 1
+    assert b._data is None
+    assert b.data is arr
+    assert callback.n_calls == 2
+
+    fn = tmp_path / "test.b"
+    with generic_io.get_file(fn, mode="w") as f:
+        b.write(f)
+    assert callback.n_calls == 3
+
+    with generic_io.get_file(fn, mode="r") as f:
+        rb = block.Block(memmap=memmap, lazy_load=lazy_load)
+        rb.read(f, past_magic=False)
+        assert_array_equal(rb.data, arr)
+
+    with pytest.raises(ValueError, match=r"Block.__init__ cannot contain non-None data and a non-None data_callback"):
+        b = block.Block(data=arr, memmap=memmap, lazy_load=lazy_load, data_callback=callback)
+
+    rb = block.Block(memmap=memmap, lazy_load=lazy_load, data_callback=callback)
+    with pytest.raises(RuntimeError, match=r"read called on a Block with a data_callback"), generic_io.get_file(
+        fn,
+        mode="r",
+    ) as f:
+        rb.read(f, past_magic=False)
