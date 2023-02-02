c47446f12d4527ca0c493d4f8441aa424adaa0ad
Brett <brettgraham@gmail.com>
2023-02-02 15:17:57 -0500

    check for deprecation warning, increase coverage

--- asdf/tests/test_asdf.py
+++ asdf/tests/test_asdf.py
@@ -417,3 +417,9 @@ def test_fsspec_http(httpserver):
     with fsspec.open(fn) as f:
         af = open_asdf(f)
         assert_tree_match(tree, af.tree)
+
+
+def test_blocks_deprecated():
+    af = AsdfFile()
+    with pytest.deprecated_call():
+        af.blocks
