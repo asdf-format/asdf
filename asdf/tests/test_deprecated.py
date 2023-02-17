2026243c65677c77ad3d7ce523fa3ca571bdd632
Brett Graham <brettgraham@gmail.com>
2023-02-17 14:20:56 -0500

    Backport PR #1337: deprecate AsdfInFits

--- asdf/tests/test_deprecated.py
+++ asdf/tests/test_deprecated.py
@@ -1,3 +1,5 @@
+import sys
+
 import pytest
 
 from asdf.exceptions import AsdfDeprecationWarning
@@ -9,3 +11,10 @@ def test_custom_type_warning():
 
         class NewCustomType(CustomType):
             pass
+
+
+def test_asdf_in_fits_import_warning():
+    if "asdf.fits_embed" in sys.modules:
+        del sys.modules["asdf.fits_embed"]
+    with pytest.warns(AsdfDeprecationWarning, match="AsdfInFits has been deprecated.*"):
+        import asdf.fits_embed  # noqa: F401
