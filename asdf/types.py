c0556550f4be83c610f29bdb6e8eb5c051366c74
Brett <brettgraham@gmail.com>
2023-02-20 12:35:06 -0500

    overwrite class modules in types deprecation

--- asdf/types.py
+++ asdf/types.py
@@ -14,4 +14,13 @@ warnings.warn(
 
 # overwrite the hidden module __file__ so pytest doesn't throw an ImportPathMismatchError
 _types.__file__ = __file__
+# overwrite the module name for each defined class to allow doc references to work
+for class_ in [
+    _types.ExtensionTypeMeta,
+    _types.AsdfTypeMeta,
+    _types.ExtensionType,
+    _types.AsdfType,
+    _types.CustomType,
+]:
+    class_.__module__ = __name__
 sys.modules[__name__] = _types
