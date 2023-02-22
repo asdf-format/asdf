6e364c7eff89e85d057846e5ce62d07dcd108f2d
William Jamieson <wjamieson@stsci.edu>
2023-02-22 13:25:12 -0500

    Backport PR #1426: Fix for missed warning supression

--- asdf/tests/test_file_format.py
+++ asdf/tests/test_file_format.py
@@ -150,6 +150,7 @@ def test_empty_file():
 
 
 @pytest.mark.filterwarnings("ignore::astropy.io.fits.verify.VerifyWarning")
+@pytest.mark.filterwarnings("ignore::asdf.exceptions.AsdfDeprecationWarning")
 def test_not_asdf_file():
     buff = io.BytesIO(b"SIMPLE")
     buff.seek(0)
