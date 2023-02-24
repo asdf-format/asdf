8bf350c09aa9dfc6902feed95a137baacdfb1971
Brett <brettgraham@gmail.com>
2023-02-24 08:35:33 -0500

    modify publicizing of resolver and type_index

--- asdf/resolver.py
+++ asdf/resolver.py
@@ -1,16 +1,27 @@
-import sys
 import warnings
 
 from . import _resolver
 from .exceptions import AsdfDeprecationWarning
 
-warnings.warn(
-    "asdf.resolver is deprecated "
-    "Please see Resources "
-    "https://asdf.readthedocs.io/en/stable/asdf/extending/resources.html",
-    AsdfDeprecationWarning,
-)
 
-# overwrite the hidden module __file__ so pytest doesn't throw an ImportPathMismatchError
-_resolver.__file__ = __file__
-sys.modules[__name__] = _resolver
+def _warn():
+    warnings.warn(
+        "asdf.resolver is deprecated "
+        "Please see Resources "
+        "https://asdf.readthedocs.io/en/stable/asdf/extending/resources.html",
+        AsdfDeprecationWarning,
+    )
+
+
+_warn()
+
+
+def __getattr__(name):
+    attr = getattr(_resolver, name)
+    _warn()
+    attr.__module__ = __name__
+    return attr
+
+
+def __dir__():
+    return dir(_resolver)
