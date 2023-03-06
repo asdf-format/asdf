76b28e54de07b8644f44e63f1b7944a757a67794
Brett <brettgraham@gmail.com>
2023-03-06 12:47:25 -0500

    remove top level legacy extension deprecated modules

--- asdf/_tests/test_deprecated.py
+++ asdf/_tests/test_deprecated.py
@@ -21,47 +21,10 @@ def test_custom_type_warning():
             pass
 
 
-def test_resolver_module_deprecation():
-    with pytest.warns(AsdfDeprecationWarning, match="^asdf.resolver is deprecated.*$"):
-        # importlib.reload doesn't appear to work here likely because of the
-        # sys.module and __file__ changes in asdf.resolver
-        if "asdf.resolver" in sys.modules:
-            del sys.modules["asdf.resolver"]
-        import asdf.resolver
-    # resolver does not define an __all__ so we will define one here
-    # for testing purposes
-    resolver_all = [
-        "Resolver",
-        "ResolverChain",
-        "DEFAULT_URL_MAPPING",
-        "DEFAULT_TAG_TO_URL_MAPPING",
-        "default_url_mapping",
-        "default_tag_to_url_mapping",
-        "default_resolver",
-    ]
-    for attr in dir(asdf.resolver):
-        if attr not in resolver_all:
-            continue
-        with pytest.warns(AsdfDeprecationWarning, match="^asdf.resolver is deprecated.*$"):
-            getattr(asdf.resolver, attr)
-
-
 def test_assert_extension_correctness_deprecation():
     extension = CustomExtension()
     with pytest.warns(AsdfDeprecationWarning, match="assert_extension_correctness is deprecated.*"):
         assert_extension_correctness(extension)
-
-
-def test_type_index_module_deprecation():
-    with pytest.warns(AsdfDeprecationWarning, match="^asdf.type_index is deprecated.*$"):
-        # importlib.reload doesn't appear to work here likely because of the
-        # sys.module and __file__ changes in asdf.type_index
-        if "asdf.type_index" in sys.modules:
-            del sys.modules["asdf.type_index"]
-        import asdf.type_index
-    for attr in asdf.type_index.__all__:
-        with pytest.warns(AsdfDeprecationWarning, match="^asdf.type_index is deprecated.*$"):
-            getattr(asdf.type_index, attr)
 
 
 @pytest.mark.parametrize("attr", ["url_mapping", "tag_mapping", "resolver", "extension_list", "type_index"])
@@ -78,16 +41,6 @@ def test_asdfile_run_hook_deprecation():
 def test_asdfile_run_modifying_hook_deprecation():
     with asdf.AsdfFile() as af, pytest.warns(AsdfDeprecationWarning, match="AsdfFile.run_modifying_hook is deprecated"):
         af.run_modifying_hook("foo")
-
-
-def test_types_module_deprecation():
-    with pytest.warns(AsdfDeprecationWarning, match="^asdf.types is deprecated.*$"):
-        if "asdf.types" in sys.modules:
-            del sys.modules["asdf.types"]
-        import asdf.types
-    for attr in asdf.types.__all__:
-        with pytest.warns(AsdfDeprecationWarning, match="^asdf.types is deprecated.*$"):
-            getattr(asdf.types, attr)
 
 
 def test_default_extensions_deprecation():
