8186cd28c9c421d22de8bb808ebe5fdc389a7955
Brett <brettgraham@gmail.com>
2023-05-01 12:05:58 -0400

    add tests for issues
    1013
    1505
    1520
    1523
    1526
    1530

---
+++ asdf/_tests/_issues/test_1520.py
@@ -0,0 +1,35 @@
+import numpy as np
+
+import asdf
+
+
+def test_1520(tmp_path):
+    """
+    A failed update can corrupt the file
+
+    https://github.com/asdf-format/asdf/issues/1520
+    """
+    fn = tmp_path / "test.asdf"
+    n_arrays = 10
+    array_size = 10000
+
+    # make a tree with many arrays that will compress well
+    af = asdf.AsdfFile()
+    for i in range(n_arrays):
+        af[i] = np.zeros(array_size, dtype="uint8") + i
+        af.set_array_compression(af[i], "zlib")
+    af.write_to(fn)
+
+    with asdf.open(fn, mode="rw") as af:
+        # now make the data difficult to compress
+        for i in range(n_arrays):
+            assert np.all(af[i] == i)
+            af[i][:] = np.random.randint(255, size=array_size)
+            af[i][0] = i + 1
+        # this no longer causes update to fail
+        assert False
+        af.update()
+
+    with asdf.open(fn, mode="r") as af:
+        for i in range(n_arrays):
+            assert af[i][0] == i + 1
---
+++ asdf/_tests/_issues/test_1526.py
@@ -0,0 +1,34 @@
+import os
+
+import numpy as np
+
+import asdf
+
+
+def test_1526(tmp_path):
+    """
+    Rewriting a file with external blocks fails if arrays are not first accessed
+
+    https://github.com/asdf-format/asdf/issues/1526
+    """
+    arrs = [np.arange(3) + i for i in range(3)]
+    af = asdf.AsdfFile({"arrs": arrs})
+    [af.set_array_storage(a, "external") for a in arrs]
+
+    dns = []
+    for i in range(2):
+        dn = tmp_path / f"d{i}"
+        if not os.path.exists(dn):
+            os.makedirs(dn)
+        dns.append(dn)
+    fns = [dn / "test.asdf" for dn in dns]
+
+    # write to d0
+    af.write_to(fns[0])
+
+    with asdf.open(fns[0]) as af2:
+        af2["arrs"][0] = 42
+        af2.write_to(fns[1])
+
+    for dn in dns:
+        assert len(os.listdir(dn)) == 4
