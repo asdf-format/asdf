244522e179e77b558e2551ee58a615d7dae599e4
Brett <brettgraham@gmail.com>
2023-05-11 10:38:49 -0400

    skip 1542 test on windows
    os.pipe returns files that windows says
    are seekable (but really aren't)
    https://bugs.python.org/issue42602

--- asdf/_tests/_issues/test_1542.py
+++ asdf/_tests/_issues/test_1542.py
@@ -1,10 +1,15 @@
 import os
+import sys
 
 import numpy as np
+import pytest
 
 import asdf
 
 
+@pytest.mark.skipif(
+    sys.platform.startswith("win"), reason="os.pipe.seek noop on windows: https://bugs.python.org/issue42602"
+)
 def test_1542():
     """
     ASDF fails to write blocks to non-seekable file
