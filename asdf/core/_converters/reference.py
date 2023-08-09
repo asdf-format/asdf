335b6780da313851a636a0801f9f842263f7c9f3
Brett <brettgraham@gmail.com>
2023-08-09 11:30:22 -0400

    fix reference uri resolution

--- asdf/core/_converters/reference.py
+++ asdf/core/_converters/reference.py
@@ -8,7 +8,12 @@ class ReferenceConverter(Converter):
     def to_yaml_tree(self, obj, tag, ctx):
         from asdf.generic_io import relative_uri
 
-        uri = relative_uri(ctx.url, obj._uri) if ctx.url is not None else obj._uri
+        base_uri = None
+        if ctx._blocks._write_fd is not None and ctx._blocks._write_fd.uri is not None:
+            base_uri = ctx._blocks._write_fd.uri
+        elif ctx.url is not None:
+            base_uri = ctx.url
+        uri = relative_uri(base_uri, obj._uri) if base_uri is not None else obj._uri
         return {"$ref": uri}
 
     def from_yaml_tree(self, node, tag, ctx):
