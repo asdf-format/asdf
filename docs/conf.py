c76fa8c4c420597989102bbd314fee7d6217423b
alphasentaurii <rkein@stsci.edu>
2025-01-27 14:23:33 -0500

    fix favicon path

--- docs/conf.py
+++ docs/conf.py
@@ -87,7 +87,7 @@ html_sidebars = {}
 # The name of an image file (within the static path) to use as favicon of the
 # docs.  This file should be a Windows icon file (.ico) being 16x16 or 32x32
 # pixels large.
-html_favicon = "_static/images/favicon.ico"
+html_favicon = "images/favicon.ico"
 html_logo = ""
 
 globalnavlinks = {
