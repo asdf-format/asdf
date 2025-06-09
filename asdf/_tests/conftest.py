eaff2907a7a1acfac8c79623dfbcd6abb39404f1
Brett <brettgraham@gmail.com>
2025-06-09 15:23:40 -0400

    add dump unit tests, consolidate test trees

--- asdf/_tests/conftest.py
+++ asdf/_tests/conftest.py
@@ -1,21 +1,56 @@
 import importlib.resources
 
+import numpy as np
 import pytest
 
 from asdf import config, schema
 
-from . import create_large_tree, create_small_tree
 from .httpserver import HTTPServer
 
 
 @pytest.fixture()
 def small_tree():
-    return create_small_tree()
+    x = np.arange(0, 10, dtype=float)
+    return {
+        "science_data": x,
+        "subset": x[3:-3],
+        "skipping": x[::2],
+        "not_shared": np.arange(10, 0, -1, dtype=np.uint8),
+    }
 
 
 @pytest.fixture()
 def large_tree():
-    return create_large_tree()
+    # These are designed to be big enough so they don't fit in a
+    # single block, but not so big that RAM/disk space for the tests
+    # is enormous.
+    x = np.zeros((256, 256))
+    y = np.ones((16, 16, 16))
+    return {
+        "science_data": x,
+        "more": y,
+    }
+
+
+@pytest.fixture
+def recursive_tree(small_tree):
+    a = small_tree.copy()
+    a["a"] = a
+    return a
+
+
+@pytest.fixture(
+    params=[
+        "small_tree",
+        "large_tree",
+        "recursive_tree",
+    ]
+)
+def tree(request):
+    """
+    Metafixture for all tree fixtures.
+    """
+    return request.getfixturevalue(request.param)
 
 
 @pytest.fixture(autouse=True)
