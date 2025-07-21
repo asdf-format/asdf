9fc1e9873b62eb391045be69189f7d45880c387a
Brett <brettgraham@gmail.com>
2025-07-21 11:08:49 -0400

    improve info help

--- asdf/_commands/info.py
+++ asdf/_commands/info.py
@@ -15,14 +15,27 @@ class Info(Command):
         parser = subparsers.add_parser(
             "info",
             help="Print a rendering of an ASDF tree.",
-            description="Print a rendering of an ASDF tree.",
+            description="""Show the contents of an ASDF file by rendering tree contents as text.
+            For large files max-rows and max-cols can be used to truncate the output to improve
+            readability. When a file contains more lines to display than max-rows the
+            deepest parts of the tree will be hidden with a message saying how many
+            rows are hidden.""",
         )
 
         parser.add_argument("filename", help="ASDF file to render")
-        parser.add_argument("--max-rows", type=int, help="maximum number of lines")
-        parser.add_argument("--max-cols", type=int, help="maximum length of line")
+        parser.add_argument(
+            "--max-rows", type=int, help="Maximum number of lines to print. If not provided all lines will be shown."
+        )
+        parser.add_argument(
+            "--max-cols", type=int, help="Maximum length of line. If not provided lines will have no length limit."
+        )
 
-        parser.add_argument("--show-values", dest="show_values", action="store_true")
+        parser.add_argument(
+            "--show-values",
+            dest="show_values",
+            action="store_true",
+            help="Display primitive values in the rendered tree (by default, enabled).",
+        )
         parser.add_argument("--no-show-values", dest="show_values", action="store_false")
         parser.set_defaults(show_values=True)
 
