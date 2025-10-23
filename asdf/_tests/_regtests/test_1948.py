498dcfca5c645e6b3ad59effe7e11f6d6b91e132
Brett <brettgraham@gmail.com>
2025-10-23 10:24:01 -0400

    rename test objects

--- asdf/_tests/_regtests/test_1948.py
+++ asdf/_tests/_regtests/test_1948.py
@@ -1,124 +1,133 @@
+"""
+Test that warn_on_failed_conversion works as expected for:
+    - lazy and non-lazy trees
+    - nested objects
+    - generator producing (yielding) converters
+
+Broken out into a regtest since there is significant
+test setup.
+
+https://github.com/asdf-format/asdf/issues/1948
+"""
+
 import pytest
 
 import asdf
 
-foo_tag_uri = "asdf://somewhere.org/tags/foo-1.0.0"
-bar_tag_uri = "asdf://somewhere.org/tags/bar-1.0.0"
-bam_tag_uri = "asdf://somewhere.org/tags/bam-1.0.0"
+test_dict_tag_uri = "asdf://somewhere.org/tags/test_dict-1.0.0"
+failing_dict_tag_uri = "asdf://somewhere.org/tags/failing_dict-1.0.0"
+failing_yield_dict_tag_uri = "asdf://somewhere.org/tags/failing_yield_dict-1.0.0"
 
 
-class Foo:
+class MyDict:
     def __init__(self, data):
         self.data = data
 
 
-class Bar(Foo):
+class FailingDict(MyDict):
     pass
 
 
-class Bam(Foo):
+class FailingYieldDict(MyDict):
     pass
 
 
-class FooConverter:
-    tags = [foo_tag_uri]
-    types = [Foo]
+class MyDictConverter:
+    tags = [test_dict_tag_uri]
+    types = [MyDict]
     lazy = True
 
     def to_yaml_tree(self, obj, tag, ctx):
         return obj.data
 
     def from_yaml_tree(self, node, tag, ctx):
-        return Foo(node)
+        return MyDict(node)
 
 
-class BarConverter:
-    tags = [bar_tag_uri]
-    types = [Bar]
+class FailingDictConverter:
+    tags = [failing_dict_tag_uri]
+    types = [FailingDict]
     lazy = True
 
     def to_yaml_tree(self, obj, tag, ctx):
         return obj.data
 
     def from_yaml_tree(self, node, tag, ctx):
-        # always make reading a bar tag fail
-        raise Exception("No bar")
+        # always make reading a failing_dict tag fail
+        raise Exception("FailingDict failed")
 
 
-class BamConverter:
-    tags = [bam_tag_uri]
-    types = [Bam]
+class FailingYieldDictConverter:
+    tags = [failing_yield_dict_tag_uri]
+    types = [FailingYieldDict]
     lazy = True
 
     def to_yaml_tree(self, obj, tag, ctx):
         return obj.data
 
     def from_yaml_tree(self, node, tag, ctx):
-        raise Exception("No bam")
+        raise Exception("FailingYieldDict failed")
         yield {}
 
 
-class FooBarExtension:
+class TestExtension:
     extension_uri = "asdf://somewhere.org/extensions/minimum-1.0.0"
-    converters = [FooConverter(), BarConverter(), BamConverter()]
-    tags = [foo_tag_uri, bar_tag_uri, bam_tag_uri]
+    converters = [MyDictConverter(), FailingDictConverter(), FailingYieldDictConverter()]
+    tags = [test_dict_tag_uri, failing_dict_tag_uri, failing_yield_dict_tag_uri]
 
 
 @pytest.fixture()
-def foobar_extension():
+def enable_test_extension():
     with asdf.config_context() as cfg:
-        cfg.add_extension(FooBarExtension())
+        cfg.add_extension(TestExtension())
         yield
 
 
-def test_failed_conversion_warns(foobar_extension):
-    """
-    Test that warn_on_failed_conversion works as expected
-    for both lazy and non-lazy trees.
-
-    Broken out into a regtest since there is significant
-    test setup.
-
-    https://github.com/asdf-format/asdf/issues/1948
-    """
-    tree = {"foo": Foo({"bar": Bar({"a": 1})}), "bam": Bam({"b": 2})}
+def test_failed_conversion_warns(enable_test_extension):
+    tree = {
+        "test_dict": MyDict({"failing_dict": FailingDict({"a": 1})}),
+        "failing_yield_dict": FailingYieldDict({"b": 2}),
+    }
     asdf_str = asdf.dumps(tree)
     with asdf.config_context() as cfg:
         cfg.warn_on_failed_conversion = True
         with (
-            pytest.warns(asdf.exceptions.AsdfConversionWarning, match="No bar"),
-            pytest.warns(asdf.exceptions.AsdfConversionWarning, match="No bam"),
+            pytest.warns(asdf.exceptions.AsdfConversionWarning, match="FailingDict failed"),
+            pytest.warns(asdf.exceptions.AsdfConversionWarning, match="FailingYieldDict failed"),
         ):
             read_obj = asdf.loads(asdf_str)
-        foo = read_obj["foo"]
-        assert isinstance(foo, Foo)
-        bar = read_obj["foo"].data["bar"]
-        assert isinstance(bar, asdf.tagged.TaggedDict)
-        assert bar._tag == bar_tag_uri
-        assert bar["a"] == 1
-        bam = read_obj["bam"]
-        assert isinstance(bam, asdf.tagged.TaggedDict)
-        assert bam._tag == bam_tag_uri
-        assert bam["b"] == 2
+        test_dict = read_obj["test_dict"]
+        assert isinstance(test_dict, MyDict)
+        failing_dict = read_obj["test_dict"].data["failing_dict"]
+        assert isinstance(failing_dict, asdf.tagged.TaggedDict)
+        assert failing_dict._tag == failing_dict_tag_uri
+        assert failing_dict["a"] == 1
+        failing_yield_dict = read_obj["failing_yield_dict"]
+        assert isinstance(failing_yield_dict, asdf.tagged.TaggedDict)
+        assert failing_yield_dict._tag == failing_yield_dict_tag_uri
+        assert failing_yield_dict["b"] == 2
 
 
-def test_lazy_failed_conversion_warns(foobar_extension, tmp_path):
+def test_lazy_failed_conversion_warns(enable_test_extension, tmp_path):
     test_path = tmp_path / "test.asdf"
-    tree = {"foo": Foo({"bar": Bar({"a": 1})}), "bam": Bam({"b": 2})}
+    tree = {
+        "test_dict": MyDict({"failing_dict": FailingDict({"a": 1})}),
+        "failing_yield_dict": FailingYieldDict({"b": 2}),
+    }
     asdf.dump(tree, test_path)
     with asdf.config_context() as cfg:
         cfg.warn_on_failed_conversion = True
         cfg.lazy_tree = True
         with asdf.open(test_path) as af:
-            foo = af["foo"]
-            assert isinstance(foo, Foo)
-            with pytest.warns(asdf.exceptions.AsdfConversionWarning, match="No bar"):
-                bar = af["foo"].data["bar"]
-            with pytest.warns(asdf.exceptions.AsdfConversionWarning, match="No bam"):
-                bam = af["bam"]
-            assert isinstance(bar, asdf.tagged.TaggedDict)
-            assert bar._tag == bar_tag_uri
-            assert bar["a"] == 1
-            assert isinstance(bam, asdf.tagged.TaggedDict)
-            assert bam._tag == bam_tag_uri
-            assert bam["b"] == 2
+            test_dict = af["test_dict"]
+            assert isinstance(test_dict, MyDict)
+            with pytest.warns(asdf.exceptions.AsdfConversionWarning, match="FailingDict failed"):
+                failing_dict = af["test_dict"].data["failing_dict"]
+            with pytest.warns(asdf.exceptions.AsdfConversionWarning, match="FailingYieldDict failed"):
+                failing_yield_dict = af["failing_yield_dict"]
+            assert isinstance(failing_dict, asdf.tagged.TaggedDict)
+            assert failing_dict._tag == failing_dict_tag_uri
+            assert failing_dict["a"] == 1
+            assert isinstance(failing_yield_dict, asdf.tagged.TaggedDict)
+            assert failing_yield_dict._tag == failing_yield_dict_tag_uri
+            assert failing_yield_dict["b"] == 2
